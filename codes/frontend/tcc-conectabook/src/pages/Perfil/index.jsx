d900e8ec6d9f185643d922c136ef96d7a4d8aa88
juliocesar-codes <cesar-pb@hotmail.com>
2026-05-07 23:04:17 -0300

    Update

--- codes/frontend/tcc-conectabook/src/pages/Perfil/index.jsx
+++ codes/frontend/tcc-conectabook/src/pages/Perfil/index.jsx
@@ -19,36 +19,34 @@ function Perfil() {
         nome: "",
         email: "",
         senha: "",
-        nascimento: ""
+        dataNascimento: ""
     })
 
     const INPUT_DATA = [
         { id: 1, name: "username", label: "Nome de Usuário", placeholder: "Digite seu usuário...", type: "text", required: true },
         { id: 2, name: "nome", label: "Nome Completo", placeholder: "Digite seu nome...", type: "text", required: true },
         { id: 3, name: "email", label: "E-mail", placeholder: "Digite seu e-mail...", type: "email", required: true },
-        { id: 4, name: "senha", label: "Senha", placeholder: "Digite sua senha...", type: "password", required: true },
         { id: 5, name: "dataNascimento", label: "Data de Nascimento", type: "date", required: true },
     ]
 
     const [user, setUser] = useState(null)
 
     useEffect(() => {
-        const userStorage = JSON.parse(localStorage.getItem("user"))
+    const userStorage = JSON.parse(localStorage.getItem("user"))
 
-        if (userStorage) {
-            setUser(userStorage)
+    if (userStorage) {
+    
+        setUser(userStorage)
 
-
-            setFormData({
-                username: userStorage.username || "",
-                nome: userStorage.nome || "",
-                email: userStorage.email || "",
-                senha: userStorage.senha || "",
-                dataNascimento: userStorage.dataNascimento || ""
-            })
-
-        }
-    }, [])
+        setFormData({
+            username: userStorage.user.nome_usuario || "",
+            nome: userStorage.user.nome || "",
+            email: userStorage.user.email || "",
+            senha: "",
+            dataNascimento: userStorage.user.data_nascimento || ""
+        })
+    }
+}, [])
 
     function handleChange(e) {
         const { name, value } = e.target
@@ -74,27 +72,10 @@ function Perfil() {
                 <div className="left-perfil">
                     <div className="user-icon">
                         <img className="img-user" src={user?.foto} alt="Foto do usuário" />
-                        <h2>{user?.nome}</h2>
-                        <p>@{user?.username}</p>
+                        <h2>{user?.user.nome}</h2>
+                        <p>@{user?.user.nome_usuario}</p>
                     </div>
 
-                    <div className="informacoes-perfil">
-                        <div className="info-perfil">
-                            <FontAwesomeIcon className="icone-perfil" icon={faBook} />
-                            <div className="livros-lido-text">
-                                <p className="stats-livro">{user?.stats.livrosLidos}</p>
-                                <p>Livros Lidos</p>
-                            </div>
-                        </div>
-
-                        <div className="info-perfil">
-                            <FontAwesomeIcon className="icone-perfil" icon={faStar} />
-                            <div className="livros-lido-text">
-                                <p className="stats-livro">{user?.stats.resenhas}</p>
-                                <p>Resenhas Publicadas</p>
-                            </div>
-                        </div>
-                    </div>
 
                     <div className="excluir-conta">
                         <Button className="button-excluir" text={"Excluir Conta"} />
@@ -113,7 +94,7 @@ function Perfil() {
                     <br />
                     <div className="inputs-perfil">
                         {INPUT_DATA.map((input) => (
-                            <div className="input-perfil">
+                            <div key={input.id} className="input-perfil">
                                 <Input
                                     name={input.name}
                                     label={input.label}
