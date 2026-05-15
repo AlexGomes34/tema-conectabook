604d74b2c65faec44d6fe59f98558eb85b4f638b
juliocesar-codes <juliocesarsantanaalves26@gmail.com>
2026-05-15 14:27:12 -0300

    Post clubes funcionando

--- codes/frontend/tcc-conectabook/src/pages/CriarClube/criarClube.jsx
+++ codes/frontend/tcc-conectabook/src/pages/CriarClube/criarClube.jsx
@@ -4,14 +4,122 @@ import Button from "../../components/button";
 import Input from "../../components/input";
 
 import "./style.css"
+import { useEffect, useState } from "react";
 
 const INPUT_DATA = [
-    { id: 1, name: "nomeClube", label: "Nome do Clube", placeholder: "Digite o nome do clube...", type: "text", required: true },
+    { id: 1, name: "nome", label: "Nome do Clube", placeholder: "Digite o name do clube...", type: "text", required: true },
     { id: 2, name: "sobre", label: "Sobre", placeholder: "Descreva o clube...", type: "text", required: true },
-    { id: 3, name: "regras", label: "Regras", placeholder: "Descreva as regras do clube...", type: "email", required: true },
+    { id: 3, name: "regras", label: "Regras", placeholder: "Descreva as regras do clube...", type: "text", required: true },
 ]
 
+const API_GENEROS        = "http://localhost:8080/v1/conectaBook/generos"
+const API_CLUBES         = "http://localhost:8080/v1/conectaBook/clubes"
+
 export default function CriarClube() {
+
+    function verificarDuplicados(clubes, form) {
+        const clubeExiste = clubes.some(
+            c => c.nome === form.nome
+        )
+
+        if(clubeExiste){
+            return "Clube existente"
+        }
+
+        return null
+    }
+
+    function validarFormulario() {
+        const {nome, sobre,regras, id_genero} = form
+
+        if(!nome.trim()) return "Nome obrigatório"
+        if(!sobre.trim()) return "Nome obrigatório"
+        if(!regras.trim()) return "Nome obrigatório"
+        if(!id_genero.trim()) return "Nome obrigatório"
+
+        return null
+    }
+
+    function handleChange(e) {
+        const {name, value} = e.target
+
+        setForm({
+            ...form,
+            [name]: value
+        })
+    }
+
+    async function handleSubmit(e) {
+        e.preventDefault()
+
+        const erro = validarFormulario()
+
+        if(erro){
+            alert(erro)
+            return
+        }
+
+        try {
+            const responseClubes = await fetch("http://localhost:8080/v1/conectaBook/clubes")
+            const dataClubes = await responseClubes.json()
+
+            const erroDuplicado = verificarDuplicados(dataClubes.response, form)
+
+            if(erroDuplicado){
+                alert(erroDuplicado)
+                return
+            }
+
+            const responseClube = await fetch(API_CLUBES, {
+                method: "POST",
+                headers:{
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify({
+                    nome: form.nome,
+                    sobre: form.sobre,
+                    regras: form.regras,
+                    id_genero: form.id_genero
+                })
+            })
+
+            if(!responseClube.ok){
+                throw new Error("Erro ao cadastrar clube");
+            }
+
+
+            alert("Clube criado com sucesso")
+
+        } catch (error) {
+            console.log(error)
+            alert("Erro no servidor")
+            
+        }
+    }
+
+    const [form, setForm] = useState({
+        nome: "",
+        sobre: "",
+        regras: "",
+        id_genero: ""
+    })
+
+    const [generos, setGeneros] = useState([])
+
+    useEffect(() => {
+        async function buscarGeneros() {
+            try {
+                const response = await fetch(API_GENEROS)
+                const data = await response.json()
+
+                setGeneros(data.response)
+            } catch (error) {
+                console.log("Erro ao buscar generos", error)
+            }
+        }
+
+        buscarGeneros()
+    }, [])
 
     return (
         <div className="container-criarClube">
@@ -19,7 +127,7 @@ export default function CriarClube() {
             <main className="main-criarClube">
                 <div className="criarClube-card">
 
-                    <form className="formulario-criarClube" action="">
+                    <form className="formulario-criarClube" onSubmit={handleSubmit}>
                         <div className="titulo-criarClube">
                             <h2>Criar Clube</h2>
                             <p>Preencha as informações a seguir:</p>
@@ -41,11 +149,13 @@ export default function CriarClube() {
 
                                 <div className="comboBox">
                                     <h2>Genero</h2>
-                                    <select name="" id="">
+                                    <select name="id_genero" value={form.id_genero} onChange={handleChange}>
                                         <option value="">Selecione uma opção</option>
-                                        <option value="acao">Ação</option>
-                                        <option value="terror">Terror</option>
-                                        <option value="aventura">Aventura</option>
+                                        {generos.map((genero)=>(
+                                            <option key={genero.id_genero} value={genero.id_genero}>
+                                                {genero.nome}
+                                            </option>
+                                        ))}
                                     </select>
                                 </div>
 
@@ -58,8 +168,8 @@ export default function CriarClube() {
                                         name={input.name}
                                         label={input.label}
                                         placeholder={input.placeholder}
-                                        value={input.value}
-                                        onChange={""}
+                                        value={form[input.name]}
+                                        onChange={handleChange}
                                         type={input.type}
                                         required={input.required}
                                     />
